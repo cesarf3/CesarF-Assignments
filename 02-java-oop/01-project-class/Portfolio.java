c0fa2495fe7bdf23aa2072bbfdc9e2b2dc19d1bd
Cesar C. F <cafra3@icloud.com>
2021-02-25 13:59:30 -0600

    Project 2.25.21

--- 02-java-oop/01-project-class/Portfolio.java
+++ 02-java-oop/01-project-class/Portfolio.java
@@ -1,16 +1,29 @@
 import java.util.ArrayList;
 public class Portfolio {
     //Create a Portfolio class that will keep an ArrayList of Project in the field projects. Use generics to ensure these are Project objects.
-    ArrayList<String> projects = new ArrayList<>();
-    public Portfolio(){
-        ArrayList<String> portfolio = {getName() + " (" + getInitialCost() + "): " + getDescription()};
-        
+    ArrayList<Project> projects = new ArrayList<Project>();
+    //add new project to the array list
+    public void addProject(Project X){
+        projects.add(X);
     }
     //Add the appropriate getters/setters/constructors for this class to work
 
     // Add the getPortfolioCost method that calculates and returns the cost to buy the entire portfolio.
-
+    public double getPortfolioCost(){
+        double cost = 0;
+        for (Project project : projects) {
+            cost += project.getInitialCost();
+        }
+        return cost;
+    }
     // Add the showPortfolio method that will print all the project elevator pitches, followed by the total cost.
+    public void showPortfolio(){
+        System.out.println("Project Elevator Pitches:");
+        for (Project project : projects) {
+            System.out.println(project.elevatorPitch());
+        }
+        System.out.println("Total cost is: $" + getPortfolioCost());
+    }
 }
 
 
--- 02-java-oop/01-project-class/ProjectTest.java
+++ 02-java-oop/01-project-class/ProjectTest.java
@@ -1,15 +1,22 @@
+
 public class ProjectTest {
     public static void main(String[] args) {
         Project demo = new Project();
         Project noDescriptionProject = new Project("No Description Project");
         Project cleaning = new Project("Cleaning", "How to clean the room");
         Project erasing = new Project("Erasing", "How to erase things on paper", 27.4);
-        System.out.println(cleaning.elevatorPitch());
+        // Testing:
+        // System.out.println(cleaning.elevatorPitch());
         System.out.println(erasing.elevatorPitch());
-        System.out.println(noDescriptionProject.elevatorPitch());
-        System.out.println(demo.elevatorPitch());
+        // System.out.println(noDescriptionProject.elevatorPitch());
+        // System.out.println(demo.elevatorPitch());
 
         Portfolio portfolio = new Portfolio();
+        portfolio.addProject(demo);
+        portfolio.addProject(noDescriptionProject);
+        portfolio.addProject(cleaning);
+        portfolio.addProject(erasing);
         
+        portfolio.showPortfolio();
     }
 }
